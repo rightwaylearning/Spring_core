93ee9d4e8178c594883000158de2d5d832464a1b
right way learning <java.development.pune@gmail.com>
2023-08-25 10:14:36 +0530

    data

--- customer-crud-operation/src/main/java/com/customer/info/customercrudoperation/CustomerCrudOperationApplication.java
+++ customer-crud-operation/src/main/java/com/customer/info/customercrudoperation/CustomerCrudOperationApplication.java
@@ -1,6 +1,8 @@
 package com.customer.info.customercrudoperation;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -27,17 +29,37 @@ public class CustomerCrudOperationApplication implements ApplicationRunner{
 	
 		// single object save
 //		Customer cust = new Customer(103,"radhu","nandan",9845673890L,"raghunandan@gmail.com");
-//	    Customer saveObj=customerServices.customerSave(cust);
+//	    Customer saveObj=customerServices.save(cust);
 //	    System.out.println(saveObj);
 	
 		
-		ArrayList<Customer> list = new ArrayList<>();
-		list.add(new Customer(107,"A","B",6253763L,"ab@gmail.com"));
-		list.add(new Customer(108,"AA","BB",6253763L,"abb@gmail.com"));
-		list.add(new Customer(109,"AAA","BBB",6253763L,"abbb@gmail.com"));
+//		ArrayList<Customer> list = new ArrayList<>();
+//		list.add(new Customer(107,"A","B",6253763L,"ab@gmail.com"));
+//		list.add(new Customer(108,"AA","BB",6253763L,"abb@gmail.com"));
+//		list.add(new Customer(109,"AAA","BBB",6253763L,"abbb@gmail.com"));
+//		
+//		Iterable<Customer> savedList= customerServices.saveAll(list);
+//		System.out.println(savedList);
 		
-		Iterable<Customer> savedList= customerServices.saveBulkObjects(list);
-		System.out.println(savedList);
+//		Optional<Customer> optional =  this.customerServices.findById(102);   
+//		
+//		if(optional.isPresent()) {
+//			Customer c = optional.get();
+//			System.out.println(c);
+//		}
+//		
+//		List<Integer> list = new ArrayList<>();
+//		list.add(102); list.add(103); list.add(14);
+//		
+//		 Iterable<Customer> customers = this.customerServices.findAllById(list);
+//		
+//		 System.out.println(customers);
+		
+//		
+//		long count = this.customerServices.count();
+//		System.out.println(count);
+		
+		
 	}
 
 }
--- customer-crud-operation/src/main/java/com/customer/info/customercrudoperation/service/CustomerServicesImpl.java
+++ customer-crud-operation/src/main/java/com/customer/info/customercrudoperation/service/CustomerServicesImpl.java
@@ -1,5 +1,7 @@
 package com.customer.info.customercrudoperation.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +15,67 @@ public class CustomerServicesImpl implements CustomerServices{
 	private CustomreDao customreDao;
 
 	@Override
-	public Customer customerSave(Customer customer) {
+	public Customer save(Customer customer) {
 		return customreDao.save(customer);
 	}
 
 	@Override
-	public Iterable<Customer> saveBulkObjects(Iterable<Customer> list) {
+	public Iterable<Customer> saveAll(Iterable<Customer> list) {
 		return customreDao.saveAll(list);
+	}
+
+	@Override
+	public Optional<Customer> findById(Integer id) {
+		Optional<Customer> optional =  this.customreDao.findById(id);
+		return optional;
+	}
+
+	@Override
+	public boolean existsById(Integer id) {
+		// TODO Auto-generated method stub
+		return this.customreDao.existsById(id);
+	}
+
+	@Override
+	public Iterable<Customer> findAll() {
+		return this.customreDao.findAll();
+	}
+
+	@Override
+	public Iterable<Customer> findAllById(Iterable<Integer> ids) {
+		return this.customreDao.findAllById(ids);
+	}
+
+	@Override
+	public long count() {
+		return this.customreDao.count();
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		this.customreDao.deleteById(id);
+		
+	}
+
+	@Override
+	public void delete(Customer entity) {
+		this.customreDao.delete(entity);
+		
+	}
+
+	@Override
+	public void deleteAllById(Iterable<Integer> ids) {
+		this.customreDao.deleteAllById(ids);
+	}
+
+	@Override
+	public void deleteAll(Iterable<Customer> entities) {
+		this.customreDao.deleteAll(entities);
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		this.customreDao.deleteAll();		
 	}
 }
